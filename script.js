f456031964fd5a5c15ff4e0f5eb0aa1d12196781
cauadossantoswandscher09 <caua.wandscher@escola.pr.gov.br>
2026-05-13 12:12:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function busca_aluno() {
+    fetch('dados.json')
+    .then(response => response.json())
+    .then(ddd => {
+        tablet = document.getElementById('lugar');
+
+        ddd.forEach(pessoainha => {
+            let linha = document.createElement('tr');
+
+            linha.innerHTML =
+            "<td>" + pessoainha.nome + "</td>" +
+            "<td>" + pessoainha.idade + "</td>" +
+            "<td>" + pessoainha.personalidade + "</td>";
+            tablet.appendChild(linha);
+        });
+    })
+
+}
